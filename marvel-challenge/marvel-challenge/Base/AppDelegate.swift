88cd89d339ef33a059f5ac03990c863454eab155
Danilo Henrique <danilo.e.souza@prservicos.com.br>
2020-07-11 23:18:34 -0300

    App Delegate MainTabBar configuration

--- marvel-challenge/marvel-challenge/Base/AppDelegate.swift
+++ marvel-challenge/marvel-challenge/Base/AppDelegate.swift
@@ -21,11 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-}
-
-//MARK: Core Data
-
-extension AppDelegate {
+    
     // MARK: - Core Data stack
 
     lazy var persistentContainer: NSPersistentContainer = {
@@ -70,5 +66,4 @@ extension AppDelegate {
             }
         }
     }
-
 }
