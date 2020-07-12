8d80aa5dcefb89a3e6fb1cc5d8c6b65f331b0a99
Danilo Henrique <danilo.e.souza@prservicos.com.br>
2020-07-12 15:08:04 -0300

    added pods

---
+++ marvel-challenge/Pods/Target Support Files/Pods-marvel-challenge/Pods-marvel-challenge-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_marvel_challenge : NSObject
+@end
+@implementation PodsDummy_Pods_marvel_challenge
+@end
---
+++ marvel-challenge/Pods/Target Support Files/Pods-marvel-challengeTests/Pods-marvel-challengeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_marvel_challengeTests : NSObject
+@end
+@implementation PodsDummy_Pods_marvel_challengeTests
+@end
