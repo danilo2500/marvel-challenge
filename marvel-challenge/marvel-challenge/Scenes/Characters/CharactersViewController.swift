f1e2e48e6e3c12e5d3bc9f33b403a4a882ac91e6
Danilo Henrique <danilo.e.souza@prservicos.com.br>
2020-07-12 14:02:29 -0300

    Added tableview

--- marvel-challenge/marvel-challenge/Scenes/Characters/CharactersViewController.swift
+++ marvel-challenge/marvel-challenge/Scenes/Characters/CharactersViewController.swift
@@ -12,19 +12,44 @@ protocol CharactersDisplayLogic: AnyObject {
     func displayError(_ error: Characters.Error)
 }
 
-class CharactersViewController: UIViewController {
+class CharactersViewController: UITableViewController {
     
     var interactor: CharactersBusinessLogic?
     var router: (NSObjectProtocol & CharactersRoutingLogic & CharactersDataPassing)?
+    
+    // MARK: Constants
+    
+    let cellReuseIdentifier = String(describing: UITableViewCell.self)
+    
+    // MARK: Variables
+    
+    var viewModel: Characters.GetCharacters.ViewModel?
+    
+    // MARK: UI Elements
+    
+    lazy var searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.placeholder = "Pesquisar"
+        return searchController
+    }()
+    
     
     // MARK: View lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
         requestCharacters()
     }
     
     // MARK: Private Functions
+    
+    private func setupUI() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        navigationItem.searchController = searchController
+        title = "Hérois"
+    }
     
     private func requestCharacters() {
         interactor?.requestCharacters()
@@ -36,11 +61,38 @@ class CharactersViewController: UIViewController {
 extension CharactersViewController: CharactersDisplayLogic {
     
     func displayCharacters(viewModel: Characters.GetCharacters.ViewModel) {
-        
+        self.viewModel = viewModel
+        tableView.reloadData()
     }
     
     func displayError(_ error: Characters.Error) {
         
     }
     
+}
+
+// MARK: TableView Data Source
+
+extension CharactersViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModel?.displayedCharacters.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath)
+        let displayedCharacter = viewModel?.displayedCharacters[indexPath.row]
+        
+        cell.textLabel?.text = displayedCharacter?.name
+        
+        return cell
+    }
+}
+
+// MARK: UISearchController Delegate
+
+extension CharactersViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
 }
